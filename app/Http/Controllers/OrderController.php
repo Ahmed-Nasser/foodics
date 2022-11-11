204bccaee6145920ad87a3660144afd94eb9bb40
Ahmed-Nasser <ahmeda.eid175@gmail.com>
2022-11-11 02:35:58 +0200

    persisting order and updating stock.

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -4,48 +4,95 @@ namespace App\Http\Controllers;
 
 use App\Models\IngredientProduct;
 use App\Models\Order;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
 use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\DB;
 
 class OrderController extends Controller
 {
     public function store(Request $request)
     {
+        // Validate request payload.
         $validated = $request->validate([
             'products'             => ['array', 'required'],
             'products.*.productId' => ['required', 'string', 'exists:products,id'],
             'products.*.quantity'  => ['required', 'integer', 'min:1'],
         ]);
 
-        /**
         //store order.
-        $products = collect($validated['products'])->map(function ($product){
-            return [
-                $product['productId'] => ['quantity' => $product['quantity']]
-            ];
-        });
-
         $order = new  Order();
-
         $order->save();
 
         //store order products.
-
-        $order->products()->attach(Arr::collapse($products));
-        **/
+        $products = $this->prepareProductQuantity($validated['products']);
+        $order->products()->attach($products);
 
         //check order products (product) ingredients.
-
-        dd(IngredientProduct::WhereIn('product_id', ['aeae07ee-75e3-4f81-8b68-8329f3dbd4e1'])->get());
+        $productIngredients = $this->getProductIngredients($products);
 
         //calculate ingredient consumption for the ordered quantity
+        $orderIngredients = $this->calculateOrderIngredients($validated['products'], $productIngredients);
+        $stockIngredient = $this->getIngredientStock($orderIngredients);
 
         // update stock
+       $this->updateIngredientStock($stockIngredient, $orderIngredients);
 
-        // update stock_transaction
+        //TODO::update stock_transaction
 
+    }
 
-        Order::create([$validated]);
+    private function prepareProductQuantity(array $products): array
+    {
+        return Arr::collapse(collect($products)->map(function ($product){
+            return [
+                $product['productId'] => ['quantity' => $product['quantity']]
+            ];
+        })->toArray());
+    }
+
+    private function getProductIngredients(array $products): Collection
+    {
+        return IngredientProduct::WhereIn('product_id', array_keys($products))->get();
+    }
+
+    private function getIngredientStock(array $ingredients): \Illuminate\Support\Collection
+    {
+        return DB::table('stocks')
+            ->select('ingredient_amount', 'ingredient_id')
+            ->whereIn('ingredient_id', array_keys($ingredients))->get();
+    }
+
+    private function calculateOrderIngredients(array $products, Collection $productIngredients): array
+    {
+        $orderIngredients = [];
+        collect($products)->map(function ($product) use ($productIngredients, &$orderIngredients){
+            return collect($productIngredients)->where('product_id', $product['productId'])
+                ->map(function ($ingredient) use ($product, &$orderIngredients){
+                    if(key_exists($ingredient->ingredient_id, $orderIngredients)){
+                        $orderIngredients[$ingredient->ingredient_id] = ($orderIngredients[$ingredient->ingredient_id]) + ($ingredient->ingredient_amount * $product['quantity'] / 1000);
+                    } else{
+                        $orderIngredients[$ingredient->ingredient_id] = ($ingredient->ingredient_amount) * ($product['quantity'] / 1000);
+                    }
+                });
+        });
+
+        return $orderIngredients;
+    }
+
+    private function updateIngredientStock(\Illuminate\Support\Collection $stockIngredient, array $orderIngredients): bool
+    {
+        $stockIngredient->map(function ($stock) use ($orderIngredients){
+            if(key_exists($stock->ingredient_id, $orderIngredients)){
+                $new_amount = ($stock->ingredient_amount) - ($orderIngredients[$stock->ingredient_id]);
+                DB::table('stocks')
+                    ->where('ingredient_id', $stock->ingredient_id)
+                    ->update([
+                        'ingredient_amount' => $new_amount,
+                    ]);
+            }
+        });
+        return true;
     }
 
 
