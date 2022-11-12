8cdd28d5527ff4280c5ddee3a06b233501c76d24
Ahmed-Nasser <ahmeda.eid175@gmail.com>
2022-11-12 02:18:55 +0200

    create order and updating stock using observer and service repo pattern.

--- app/Models/Stock.php
+++ app/Models/Stock.php
@@ -6,6 +6,9 @@ use Database\Factories\StockFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Stock extends Model
 {
@@ -15,13 +18,28 @@ class Stock extends Model
 
     protected $keyType = 'string';
 
+    protected $primaryKey = 'ingredient_id';
+
     protected $fillable = [
         'ingredient_id',
         'ingredient_amount',
     ];
 
+    protected static function boot()
+    {
+        parent::boot();
+        self::updated(function ($model){
+
+        });
+    }
+
     protected static function newFactory(): StockFactory
     {
         return StockFactory::new();
+    }
+
+    public function ingredients(): BelongsTo
+    {
+        return $this->belongsTo(Ingredient::class, 'ingredient_id', 'id');
     }
 }
--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -3,7 +3,9 @@
 namespace App\Providers;
 
 use App\Models\Order;
+use App\Models\Stock;
 use App\Observers\OrderObserver;
+use App\Observers\StockObserver;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -23,6 +25,7 @@ class EventServiceProvider extends ServiceProvider
 
     protected $observers = [
         Order::class => [OrderObserver::class],
+        Stock::class => [StockObserver::class],
     ];
 
     /**
