ca964f283746803b97b534df084446311b89e292
Ahmed-Nasser <ahmeda.eid175@gmail.com>
2022-11-12 04:52:52 +0200

    create order and updating stock using observer and service repo pattern.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
+            MerchantSeeder::class,
             ProductSeeder::class,
             IngredientSeeder::class,
             StockSeeder::class,
